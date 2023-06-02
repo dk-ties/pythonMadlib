977c7a2137d6c034286d1096907163331ea0c4c3
dk-ties <dk.ties@gmail.com>
2023-06-02 20:01:46 +0200

    Setting up first few lines of the chat robot, first and last name as input

---
+++ madLib.py
@@ -0,0 +1,13 @@
+##1 Project: Mad libs Python project.
+#My Idea:
+#Make a "chat robot" that will take a few input, and play around with them.
+#Indput: Name, Last name, Age, Profession
+#Output: Hello Full name, A pressention of the age and profession. Scamble name.
+
+
+
+#The chat 'Robot'
+print('Hello and welcome to the Mad libs Chat robot \n')
+firstName = input('Let me start by kindly ask for your First Name: \n')
+lastName = input('Welll Hallo: ' + firstName + ' please what is your last name? \n')
+print('Well hallo ' + firstName + ' ' + lastName + ' How old are you?')
