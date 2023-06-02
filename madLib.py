142327cd4f4cae3244349b7b5689e6811cc2aac8
dk-ties <dk.ties@gmail.com>
2023-06-02 20:13:32 +0200

    Input age, transform to int, find birth year using import datetime

--- madLib.py
+++ madLib.py
@@ -4,10 +4,20 @@
 #Indput: Name, Last name, Age, Profession
 #Output: Hello Full name, A pressention of the age and profession. Scamble name.
 
-
+import datetime
 
 #The chat 'Robot'
+
+today = datetime.date.today()
+
+
 print('Hello and welcome to the Mad libs Chat robot \n')
 firstName = input('Let me start by kindly ask for your First Name: \n')
 lastName = input('Welll Hallo: ' + firstName + ' please what is your last name? \n')
-print('Well hallo ' + firstName + ' ' + lastName + ' How old are you?')
+inputAge = input('Well hallo ' + firstName + ' ' + lastName + ' How old are you? ')
+age = int(inputAge)
+print (type(age))
+print (type(today.year))
+yearOfBirth = str(age-today.year)
+#profession = input 
+print('Wow, so you must have been born around' + yearOfBirth + ' \n')
