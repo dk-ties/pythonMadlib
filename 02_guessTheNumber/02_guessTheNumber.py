ceb98fad4e253cb2984e89458b21ef62c605a062
dk-ties <dk.ties@gmail.com>
2023-06-18 08:44:44 +0200

    Setting up psudo for guessthenumber

---
+++ 02_guessTheNumber/02_guessTheNumber.py
@@ -0,0 +1,23 @@
+""" 2 Project: Guess the number (vs computer)
+My Idea:
+Create a small guess the number, just in terminal
+Output some print,
+Input a guess
+Output if it correct, or needs to be warmer or colder. """
+
+""" 
+computerGuess = Random number between 0-10 
+input player guess 
+    Transform to integer
+    Check if between 0-10
+    Check if is == to computerGuess
+        Player wins
+        
+    If not  
+            lowver ask for new number with message of hotter
+    if not
+            Higher ask for new number with message of colder
+    Guess correct start new game
+    
+Play up to 5 before exit game.
+"""
