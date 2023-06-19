f2b5542122c62be037da6365a6dbb4db2877f950
dk-ties <dk.ties@gmail.com>
2023-06-19 20:43:46 +0200

    Setting up a guess the number game working

--- 02_guessTheNumber/02_guessTheNumber.py
+++ 02_guessTheNumber/02_guessTheNumber.py
@@ -1,3 +1,6 @@
+import random
+
+
 """ 2 Project: Guess the number (vs computer)
 My Idea:
 Create a small guess the number, just in terminal
@@ -10,6 +13,8 @@ computerGuess = Random number between 0-10
 input player guess 
     Transform to integer
     Check if between 0-10
+Create computerGuess
+    Randoom number between 0-10 Store it.
     Check if is == to computerGuess
         Player wins
         
@@ -21,3 +26,60 @@ input player guess
     
 Play up to 5 before exit game.
 """
+
+""" playerGuess = int(input("Please write a number between 0-10 "))
+if playerGuess > 10 or playerGuess < 0:
+    print("Ohh looks like your number is not between 0-10 ")
+    playerGuess = input("Please wirte a number between 0-10 ")
+computerGuess =  random.randint(0,10)
+print(computerGuess)
+
+if playerGuess > computerGuess:
+    print("Ohh you are close are you, try a bit warmer")
+elif playerGuess < computerGuess:
+        print("Ohh close one, try a bit colder")
+elif playerGuess == computerGuess:
+        print("BOOOOM you nailed it GOOD JOB")
+quit() """
+
+def getPlayerGuess():
+    getPlayerGuessStatus = True
+    
+    while getPlayerGuessStatus == True:
+        playerGuess = input("Please write a number between 0-10 ")
+        try: 
+            print('Ohh it looks like you wrote a number')
+            playerGuess = int(playerGuess)
+            if playerGuess > 10 or playerGuess < 0:
+                    continue
+            else:
+                getPlayerGuessStatus = False
+            print(f'Here it is :) {playerGuess}')
+        except:
+            print("Ohh did you not enter a number?")
+            print(playerGuess)
+            continue
+    return(playerGuess)
+print(getPlayerGuess())
+
+
+
+gameScore = True
+computerGuess =  random.randint(0,10)
+print(computerGuess)
+gameScore = 0
+while gameScore < 5:
+        playerGuess = getPlayerGuess()
+        if playerGuess < computerGuess:
+            print("Ohh you are close are you, try a bit warmer")
+            continue
+        elif playerGuess > computerGuess:
+            print("Ohh close one, try a bit colder")
+            continue
+        elif playerGuess == computerGuess:
+            print("BOOOOM you nailed it GOOD JOB")
+            gameScore = gameScore+1
+            print(f'Score:  {gameScore}')
+            computerGuess =  random.randint(0,10)
+            print(computerGuess)
+print('And you are the BIG WINNER!!!!!')
