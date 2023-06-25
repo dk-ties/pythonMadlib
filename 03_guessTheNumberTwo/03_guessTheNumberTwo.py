efe391ede76fb733a76d922d38d6c6652686cdc5
dk-ties <dk.ties@gmail.com>
2023-06-25 19:47:56 +0200

    Setting up the player one and two arryas

--- 03_guessTheNumberTwo/03_guessTheNumberTwo.py
+++ 03_guessTheNumberTwo/03_guessTheNumberTwo.py
@@ -18,7 +18,8 @@ playerTwo = {
     "guess": 0
 }
 
-
+print(playerOne["guess"])
+print(playerTwo["guess"])
 
 
 
@@ -43,25 +44,48 @@ def getPlayerGuess():
 print(getPlayerGuess())
 
 
+""" if playerOne.round < 5 and playerTwo.round < 5:
+    gameScore = True
+ """#computerGuess =  random.randint(0,10)
 
-def main():
-    if playerOne.round > playerTwo.round:
-            playerOne
-        while gameScore < 5:
-                playerGuess = getPlayerGuess()
-                if playerGuess < computerGuess:
-                    print("Ohh you are close are you, try a bit warmer")
-                    continue
-                elif playerGuess > computerGuess:
-                    print("Ohh close one, try a bit colder")
-                    continue
-                elif playerGuess == computerGuess:
-                    print("BOOOOM you nailed it GOOD JOB")
-                    gameScore = gameScore+1
-                    print(f'Score:  {gameScore}')
-                    computerGuess =  random.randint(0,10)
-                    print(computerGuess)
-        print('And you are the BIG WINNER!!!!!')
+playerOne["guess"] = getPlayerGuess()
+playerTwo["guess"] = getPlayerGuess()
 
+while gameScore < 5:
+    if playerOne["guess"] < playerTwo["guess"]:
+        print("Ohh you are close are you, try a bit warmer")
+        continue
+    elif playerOne["guess"] > playerTwo["guess"]:
+        print("Ohh close one, try a bit colder")
+        continue
+    elif playerOne["guess"] == playerTwo["guess"]:
+        print("BOOOOM you nailed it GOOD JOB")
+        gameScore = gameScore+1
+        print(f'Score:  {gameScore}')
+        computerGuess =  random.randint(0,10)
+        print(computerGuess)
+print('And you are the BIG WINNER!!!!!')
+
+
+
+
+""" 
+gameScore = 0
+while gameScore True:
+        playerGuess = getPlayerGuess()
+        if playerGuess < computerGuess:
+            print("Ohh you are close are you, try a bit warmer")
+            continue
+        elif playerGuess > computerGuess:
+            print("Ohh close one, try a bit colder")
+            continue
+        elif playerGuess == computerGuess:
+            print("BOOOOM you nailed it GOOD JOB")
+            gameScore = gameScore+1
+            print(f'Score:  {gameScore}')
+            computerGuess =  random.randint(0,10)
+            print(computerGuess)
+print('And you are the BIG WINNER!!!!!')
+ """
 
 """ How to make this to a two player game? """
