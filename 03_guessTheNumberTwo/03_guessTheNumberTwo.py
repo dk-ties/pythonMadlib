3c0beb58b00eaf1631d4ca621df6d8057f6fbd56
dk-ties <dk.ties@gmail.com>
2023-06-28 19:45:27 +0200

    made a new game loop, with player dic working

--- 03_guessTheNumberTwo/03_guessTheNumberTwo.py
+++ 03_guessTheNumberTwo/03_guessTheNumberTwo.py
@@ -10,10 +10,12 @@ Output if it correct, or needs to be warmer or colder.
 switch round, after each succesfull guess"""
 
 playerOne = {
+    "name" : "PlayerOne",
     "round": 0 ,
     "guess": 0
 }
 playerTwo = {
+    "name" : "PlayerTwo",
     "round": 0 ,
     "guess": 0
 }
@@ -23,11 +25,11 @@ print(playerTwo["guess"])
 
 
 
-def getPlayerGuess():
+def getPlayerGuess(player):
     getPlayerGuessStatus = True
     
     while getPlayerGuessStatus == True:
-        playerGuess = input("Please write a number between 0-10 ")
+        playerGuess = input(f"Hello {player['name']} Please write a number between 0-10 ")
         try: 
             print('Ohh it looks like you wrote a number')
             playerGuess = int(playerGuess)
@@ -41,35 +43,68 @@ def getPlayerGuess():
             print(playerGuess)
             continue
     return(playerGuess)
-print(getPlayerGuess())
 
 
-""" if playerOne.round < 5 and playerTwo.round < 5:
+playerOne['guess'] = getPlayerGuess(playerOne)
+gameScore = True
+while gameScore == True:
+        playerTwo['guess'] = getPlayerGuess(playerTwo)
+        print('So far, so good', playerTwo)
+        if playerTwo['guess'] == playerOne["guess"]:
+                print('Wow spot on')
+                break
+        elif playerTwo['guess'] > playerOne["guess"]:
+                print('Ohh try a bit colder')
+                continue
+        elif playerTwo['guess'] < playerOne["guess"]:
+                print('Ohh try a bit colder')
+                continue    
+gameScore = False
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if playerOne["round"] < 5:
     gameScore = True
- """#computerGuess =  random.randint(0,10)
+#computerGuess =  random.randint(0,10)
 
-playerOne["guess"] = getPlayerGuess()
-playerTwo["guess"] = getPlayerGuess()
+    
+    playerTwo["guess"] = getPlayerGuess()
 
-while gameScore < 5:
-    if playerOne["guess"] < playerTwo["guess"]:
-        print("Ohh you are close are you, try a bit warmer")
-        continue
-    elif playerOne["guess"] > playerTwo["guess"]:
-        print("Ohh close one, try a bit colder")
-        continue
-    elif playerOne["guess"] == playerTwo["guess"]:
-        print("BOOOOM you nailed it GOOD JOB")
-        gameScore = gameScore+1
-        print(f'Score:  {gameScore}')
-        computerGuess =  random.randint(0,10)
-        print(computerGuess)
+    while gameScore < 5:
+        playerOne["guess"] = getPlayerGuess()    
+        if playerOne["guess"] < playerTwo["guess"]:
+            print("Ohh you are close are you, try a bit warmer")
+            continue
+        elif playerOne["guess"] > playerTwo["guess"]:
+            print("Ohh close one, try a bit colder")
+            continue
+        elif playerOne["guess"] == playerTwo["guess"]:
+            print("BOOOOM you nailed it GOOD JOB")
+            playerOne["round"] += 1
+            print(f'Score:  {gameScore}')
+            computerGuess =  random.randint(0,10)
+            print(playerOne["round"])
 print('And you are the BIG WINNER!!!!!')
 
 
 
-
-""" 
 gameScore = 0
 while gameScore True:
         playerGuess = getPlayerGuess()
@@ -86,6 +121,6 @@ while gameScore True:
             computerGuess =  random.randint(0,10)
             print(computerGuess)
 print('And you are the BIG WINNER!!!!!')
- """
 
-""" How to make this to a two player game? """
+
+ How to make this to a two player game? """
