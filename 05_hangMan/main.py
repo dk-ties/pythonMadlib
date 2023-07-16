2e97b72da17328d12951c394cfaaa5204bbfe2a6
dk-ties <dk.ties@gmail.com>
2023-07-16 07:27:38 +0200

    working on 05_hangman. Function to find random word, and function for getting input

--- 05_hangMan/main.py
+++ 05_hangMan/main.py
@@ -1,4 +1,35 @@
+#Imports
 import random
 from words import words
 
-print(words)
+#Variable
+numberOfGuess = 0
+
+#funtion to get a raondom word, from words.py
+def randomWord():
+    word = None
+    randomNumber = random.randint(0, len(words))
+    print(randomNumber)
+    word = list(words[randomNumber])
+    return word
+
+
+
+print(f'Hello and wellcome to a game of Hangman. The computer have chosen a word for you to Guess. the word consist of {len(wordToGuess)} charector...')
+
+#Function to get a guess.
+def getGuess(numberOfGuess):
+    
+    while True:
+        guess = input(f'Please type in your {numberOfGuess} guess ')
+        if len(guess) > 1:
+            print('Please only type one charector')
+            continue
+        else:
+            break
+    
+    numberOfGuess += 1
+    return guess
+print(numberOfGuess)
+print(getGuess(numberOfGuess))
+print(numberOfGuess)
