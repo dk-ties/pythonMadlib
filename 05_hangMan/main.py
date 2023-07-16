cb27f115403af28cbe76b277092bc37ff93e0221
dk-ties <dk.ties@gmail.com>
2023-07-16 09:24:48 +0200

    Working game! Setup game loop, tested, next step change randomword to select words < 10 or change guess counter update

--- 05_hangMan/main.py
+++ 05_hangMan/main.py
@@ -9,13 +9,12 @@ numberOfGuess = 0
 def randomWord():
     word = None
     randomNumber = random.randint(0, len(words))
-    print(randomNumber)
+    #print(randomNumber)
     word = list(words[randomNumber])
     return word
 
 
 
-print(f'Hello and wellcome to a game of Hangman. The computer have chosen a word for you to Guess. the word consist of {len(wordToGuess)} charector...')
 
 #Function to get a guess.
 def getGuess(numberOfGuess):
@@ -28,8 +27,51 @@ def getGuess(numberOfGuess):
         else:
             break
     
-    numberOfGuess += 1
+
     return guess
-print(numberOfGuess)
-print(getGuess(numberOfGuess))
-print(numberOfGuess)
+
+#Function to compare word to gues, with guess
+
+def compareGuess(guess):
+    wordPosi = None
+    try: 
+        wordPosi = wordToGuessBack.index(guess)
+        #print(f'The char is on position {wordPosi}')
+    except ValueError:
+        print('That char is not a part of the word, please try again.')
+
+
+    return wordPosi
+#Game loop starting...
+
+while True:
+    wordToGuessBack = randomWord()
+    wordtoGuessFront = ['x'*len(wordToGuessBack)]
+    wordtoGuessFront = list(wordtoGuessFront[0])
+    print(f'Backend word {wordToGuessBack}')
+    print(f'Hello and wellcome to a game of Hangman. The computer have chosen a word for you to Guess. the word consist of {wordtoGuessFront} charector...')
+    while numberOfGuess < 10:
+        #print(type(wordtoGuessFront))
+        #print(f'word to guess: {wordtoGuessFront}')        
+        #print('Please enter a gues ')
+        currentGuess = getGuess(numberOfGuess)
+        currentGuessIndex = compareGuess(currentGuess)
+        #print(f'the char you type have index number: {currentGuessIndex}')
+        numberOfGuess += 1
+        if currentGuessIndex == None:
+            print('Oh seems that the chars is not part of the word, please try again ')
+            continue
+        else:
+            print('So is im here or whats happening?')
+            print(f'so on place you guess are currently this char {wordtoGuessFront[currentGuessIndex]}', currentGuess)
+            wordtoGuessFront[currentGuessIndex] = currentGuess
+            print(wordtoGuessFront)
+            continue
+    break
+            
+print(f'We got to the end of the game, and you need {numberOfGuess} to guess the word.') 
+
+
+#print(numberOfGuess)
+#print(getGuess(numberOfGuess))
+#print(numberOfGuess)
